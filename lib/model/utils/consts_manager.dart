15f120fb27f1fdfb946ab7c687f9b2041baec02a
ahmad mriwed <mr.ahmadmriwed@gmail.com>
2023-02-05 00:05:46 +0300

    @mriwed

---
+++ lib/model/utils/consts_manager.dart
@@ -0,0 +1,45 @@
+
+
+class AppConstants {
+  static const int splashDelay = 5;
+  static const int logoDelay = 1600;
+  static const int onBoardingDelay = 500;
+  static const themeKEY = "theme";
+  static const languageKEY = "language";
+  static const isLoginedKEY = "isLogin";
+  static const nameKEY = "name";
+  static const phoneNumberKEY = "phoneNumber";
+  static const emailKey = "email";
+  static const tokenKEY = "token";
+  static const idKEY = "id";
+  static const uidKEY = "uid";
+  static const emailContact = "kholodalaskary9@gmail.com";
+
+  static String storageUrl = "gs://ehtooa-266e1.appspot.com";
+  static String photoProfileUser = "";
+  static String photoProfileAdmin = "";
+  static String photoProfileLawyer =
+      "";
+  static String photoGroup =
+      "";
+  static String imagesGroup ="Groups/Images/";
+  static String filesGroup ="Groups/Files/";
+  static String audiosGroup ="Groups/Audios/";
+  static String videosGroup ="Groups/Videos/";
+
+  //collection
+  static String collection = "";
+  static String collectionAdmin = "Admin";
+  static String collectionLawyer = "Lawyer";
+  static String collectionUser = "User";
+  static String collectionDateLawyer = "DateLawyer";
+  static String collectionInternship = "Internship";
+  static String collectionDateO = "DateO";
+  static String collectionChat = "Chat";
+  static String collectionMessage = "Message";
+
+
+
+
+
+}
