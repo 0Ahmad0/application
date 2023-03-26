f44b34d2c02acc87effb4c9e883cfedc462c19d9
VISION <ah774129@gmail.com>
2023-03-26 15:45:52 +0200

    finish all pinkey

---
+++ lib/view/translations/locale_keys.g.dart
@@ -0,0 +1,36 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const app_name = 'app_name';
+  static const app_description = 'app_description';
+  static const welcome_back = 'welcome_back';
+  static const login_to_complete_book = 'login_to_complete_book';
+  static const login_now = 'login_now';
+  static const phone_number = 'phone_number';
+  static const login_by_email = 'login_by_email';
+  static const password = 'password';
+  static const type = 'type';
+  static const forget_password = 'forget_password';
+  static const login = 'login';
+  static const do_not_have_account = 'do_not_have_account';
+  static const sign_up = 'sign_up';
+  static const loading = 'loading';
+  static const active = 'active';
+  static const non_active = 'non_active';
+  static const toast = 'toast';
+  static const toast_successfully_created = 'toast_successfully_created';
+  static const toast_successfully_update = 'toast_successfully_update';
+  static const toast_Unsuccessfully_created = 'toast_Unsuccessfully_created';
+  static const toast_email_already_use = 'toast_email_already_use';
+  static const toast_short_password = 'toast_short_password';
+  static const toast_successfully_logged = 'toast_successfully_logged';
+  static const toast_email_invalid = 'toast_email_invalid';
+  static const toast_password_invalid = 'toast_password_invalid';
+  static const toast_network_error = 'toast_network_error';
+  static const toast_account_invalid = 'toast_account_invalid';
+  static const toast_Bad_data_fetch = 'toast_Bad_data_fetch';
+  static const toast_time_out = 'toast_time_out';
+  static const toast_given_empty = 'toast_given_empty';
+  static const toast_account_not_active = 'toast_account_not_active';
+
+}
